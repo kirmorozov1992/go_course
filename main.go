7eaa7836ffae36698ca94c5817e9bf283d501588
kirmorozov1992 <kirmorozov1992@yandex.ru>
2025-10-25 20:33:53 +0600

    initial commit

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 5
+
+	fmt.Println(a)
+	fmt.Println("Hello world!")
+	fmt.Println("Hello world!")
+	fmt.Println("Hello world!")
+	fmt.Println("Hello world!")
+}
